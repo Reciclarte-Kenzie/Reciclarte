d92b322dc5b0c5bfaefb3f5406ce3dc2d62ba119
Danielle da Luz <daniln2209@gmail.com>
2023-01-03 18:07:01 +0000

    feat: add color variables

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    :root {
+        --color-green-100: #011B0C;
+        --color-green-90: #022E15;
+        --color-grey-100: #7A7A7A;
+        --color-grey-90: #969696;
+        --color-grey-70: #848484;
+        --color-grey-60: #D2D2D2;
+        --color-grey-50: #F5F5F5;
+        --color-orange-80: #F7941D;
+        --color-white: #FFFFFF;
+    }
+`
