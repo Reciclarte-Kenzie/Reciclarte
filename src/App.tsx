9eff1b5d9379e44b84c1a22341903b03e2d61c5d
Danielle da Luz <daniln2209@gmail.com>
2023-01-03 18:43:26 +0000

    feat: add global styles on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
 
 export function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <GlobalStyles />
+    </>
   )
 }
