28f1280ca2c2298c5dca6e96026901e16f28147f
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2023-01-03 18:42:02 -0300

    feat Routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes} from "react-router-dom";
+
+export const RoutesApp = () => {
+    return (
+      <Routes>
+        {/* <Route path="/" element={<Home/>}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} /> */}
+      </Routes>
+    );
+  };
+  
