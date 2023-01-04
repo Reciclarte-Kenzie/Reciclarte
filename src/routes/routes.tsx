f675e9b77b56b7ceb62ff0612465828b0fdb5199
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2023-01-04 10:52:52 -0300

    fix: rota comentada individual

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,11 +4,11 @@ import { Navigate, Route, Routes} from "react-router-dom";
 export const RoutesApp = () => {
     return (
       <Routes>
-        {/* <Route path="/" element={<Home/>}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} /> 
-        <Route path="*" element={<Navigate to="/" />}/> */}
+        {/* <Route path="/" element={<Home/>}> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="/profile" element={<Profile />} />  */}
+        {/* <Route path="*" element={<Navigate to="/" />}/> */}
       </Routes>
     );
   };
