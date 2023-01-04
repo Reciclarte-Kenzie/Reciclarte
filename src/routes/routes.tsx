e59763e88815450c12d6fed0c27d61c420b07dff
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2023-01-04 09:35:01 -0300

    fix add new route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes} from "react-router-dom";
 
 export const RoutesApp = () => {
     return (
@@ -7,7 +7,8 @@ export const RoutesApp = () => {
         {/* <Route path="/" element={<Home/>}>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} /> */}
+        <Route path="/profile" element={<Profile />} /> 
+        <Route path="*" element={<Navigate to="/" />}/> */}
       </Routes>
     );
   };
