c02e0de640850b9c8c60eddf8f74964a6f0045eb
Danielle da Luz <daniln2209@gmail.com>
2023-01-04 16:49:50 +0000

    feat: remove merge conflict message

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/userContext";
 import { RoutesApp } from "./routes/routes";
 import { GlobalStyles } from "./styles/GlobalStyles";
-<<<<<<< HEAD
-=======
->>>>>>> c3495dc73c8f6bb452b385cd6baa2dd8d2674c8f
 
 export function App() {
   return (
