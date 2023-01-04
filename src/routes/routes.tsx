6e9665b556080efa9a7bb00fbf3af205aca0bfc8
ThiagoBettinRamos <thiago_bettin@hotmail.com>
2023-01-04 13:44:26 -0300

    feat: remove conflits

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navigate, Route, Routes} from "react-router-dom";
+import { RegisterPage } from "../pages/register";
 
 export const RoutesApp = () => {
     return (
       <Routes>
         {/* <Route path="/" element={<Home/>}> */}
         {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/register" element={<RegisterPage />} />
         {/* <Route path="/profile" element={<Profile />} />  */}
         {/* <Route path="*" element={<Navigate to="/" />}/> */}
       </Routes>
