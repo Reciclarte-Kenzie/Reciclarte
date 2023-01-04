1942a0436254cddf1165b526c6077505070ba32f
Luís Nunes <luis.ebn98@gmail.com>
2023-01-04 17:23:29 -0300

    feat: add dynamic button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,14 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const StyledButton = styled.button`
-  background-color: var(--color-orange-80);
-  border: 2px solid var(--color-orange-80);
+interface iStyledButton {
+  buttonType?: string;
+  width?: string;
+  padding?: string;
+}
+
+export const StyledButton = styled.button<iStyledButton>`
   border-radius: var(--radius-1);
-  color: var(--color-white);
   font-weight: 400;
   font-size: var(--font-size-18);
-  padding: 1vh 0;
-  width: clamp(250px, 100%, 300px);
+  padding: ${(props) => (props.padding ? props.padding : "1vh 0")};
+  width: ${(props) =>
+    props.width ? props.width : "clamp(250px, 100%, 300px)"};
   transition: 300ms;
 
   :hover,
@@ -22,4 +26,27 @@ export const StyledButton = styled.button`
     border: 2px solid var(--color-grey-100);
     color: var(--color-grey-100);
   }
+
+  ${({ buttonType }) => {
+    switch (buttonType) {
+      case "invert":
+        return css`
+          background-color: var(--color-white);
+          border: 2px solid var(--color-white);
+          color: var(--color-orange-80);
+        `;
+      case "whiteBorder":
+        return css`
+          background-color: var(--color-orange-80);
+          border: 2px solid var(--color-white);
+          color: var(--color-white);
+        `;
+      default:
+        return css`
+          background-color: var(--color-orange-80);
+          border: 2px solid var(--color-orange-80);
+          color: var(--color-white);
+        `;
+    }
+  }}
 `;
