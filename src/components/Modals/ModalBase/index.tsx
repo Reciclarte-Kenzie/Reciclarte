025bee7bda3b84f7ec79a0a9208dee14e68e7fdc
Danielle da Luz <daniln2209@gmail.com>
2023-01-05 21:18:55 +0000

    feat: add modal header structure

--- src/components/Modals/ModalBase/index.tsx
+++ src/components/Modals/ModalBase/index.tsx
@@ -1,11 +1,21 @@
 import { ModalBaseStyled } from "./styles";
+import closeIcon from "../../../assets/imgs/close-icon.svg";
 
-export const ModalBase = () => {
+interface iModalBaseProps {
+  modalTitle: string;
+}
+
+export const ModalBase = ({ modalTitle }: iModalBaseProps) => {
   return (
     <ModalBaseStyled>
       <article>
-        
+        <header>
+          <h2>{modalTitle}</h2>
+          <button aria-label="fechar modal">
+            <img src={closeIcon} alt="ícone branco com um 'x' no meio" />
+          </button>
+        </header>
       </article>
     </ModalBaseStyled>
   );
-}
+};
