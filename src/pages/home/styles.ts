422e987afc8f356a685461e5a44576461528ebf3
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2023-01-05 14:37:18 -0300

    merge feat/cards

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledHome = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   background-color: var(--color-grey-50);
 
   header {
