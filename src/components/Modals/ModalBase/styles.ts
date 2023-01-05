d80617ff687b8d0177b14a012107be7c70952165
Danielle da Luz <daniln2209@gmail.com>
2023-01-05 21:10:42 +0000

    feat: add modal container style

--- src/components/Modals/ModalBase/styles.ts
+++ src/components/Modals/ModalBase/styles.ts
@@ -1,9 +1,21 @@
 import styled from "styled-components";
 
 export const ModalBaseStyled = styled.div`
+  align-items: center;
   background-color: rgba(0, 0, 0, 0.76);
+  display: flex;
   height: 100%;
-  width: 100%;
+  justify-content: center;
   position: fixed;
   z-index: 1;
+  width: 100%;
+
+  article {
+    background-color: var(--color-white);
+    border-radius: var(--radius-1);
+    max-width: 798px;
+    margin: 20px;
+    min-height: 318px;
+    width: 100%;
+  }
 `
