687190001ea559f05d60c384b5823cf58b130e52
Danielle da Luz <daniln2209@gmail.com>
2023-01-05 20:57:35 +0000

    feat: add modal base structure

---
+++ src/components/Modals/ModalBase/index.tsx
@@ -0,0 +1,11 @@
+import { ModalBaseStyled } from "./styles";
+
+export const ModalBase = () => {
+  return (
+    <div>
+      <ModalBaseStyled>
+        
+      </ModalBaseStyled>
+    </div>
+  );
+}
