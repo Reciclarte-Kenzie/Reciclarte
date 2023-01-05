37d234dc50c52016e7bc285d748e6913ac31c14f
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2023-01-05 13:15:33 -0300

    merge develop

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,1 +1,35 @@
-export {};
+import { UseFormRegisterReturn } from "react-hook-form";
+import { StyledInput, StyledLabel } from "./styles";
+
+interface iInput {
+  type: string;
+  placeholder: string;
+  label: string;
+  id: string;
+  register?: UseFormRegisterReturn;
+  disabled?: boolean;
+}
+
+export const Input = ({
+  type = "text",
+  placeholder,
+  label,
+  id,
+  register,
+  disabled,
+}: iInput) => {
+  return (
+    <fieldset>
+      <StyledLabel htmlFor={id}>{label}</StyledLabel>
+      <StyledInput
+        type={type}
+        placeholder={placeholder}
+        id={id}
+        name={id}
+        aria-label={placeholder}
+        {...register}
+        disabled={disabled}
+      />
+    </fieldset>
+  );
+};
