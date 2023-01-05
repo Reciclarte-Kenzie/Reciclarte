b902dc6702c48055c66ca16ad8389ea545bd9062
Danielle da Luz <daniln2209@gmail.com>
2023-01-05 21:03:31 +0000

    feat: add low opacity background

--- src/components/Modals/ModalBase/index.tsx
+++ src/components/Modals/ModalBase/index.tsx
@@ -2,10 +2,10 @@ import { ModalBaseStyled } from "./styles";
 
 export const ModalBase = () => {
   return (
-    <div>
-      <ModalBaseStyled>
+    <ModalBaseStyled>
+      <article>
         
-      </ModalBaseStyled>
-    </div>
+      </article>
+    </ModalBaseStyled>
   );
 }
--- src/components/Modals/ModalBase/styles.ts
+++ src/components/Modals/ModalBase/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
-export const ModalBaseStyled = styled.article`
-
+export const ModalBaseStyled = styled.div`
+  background-color: rgba(0, 0, 0, 0.76);
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  z-index: 1;
 `
