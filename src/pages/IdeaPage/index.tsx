5e101ec5e25a5542e98e07d6d311d463e62f3dd0
Olivio Freitas <ojfneto92@gmail.com>
2023-01-06 11:30:05 -0300

    feat: component lines

--- src/pages/IdeaPage/index.tsx
+++ src/pages/IdeaPage/index.tsx
@@ -1,9 +1,32 @@
-import { StyledIdeaPage } from "./StyledIdeaPage"
+import { StyledIdeaPage } from "./StyledIdeaPage";
 
 export const index = () => {
   return (
-    <StyledIdeaPage>
-        <h2>HEADER</h2>
-    </StyledIdeaPage>
-  )
-}
+    <>
+      <h2>HEADER</h2>
+      <StyledIdeaPage>
+        <figure>
+          <img src="" alt="Imagem da API" />
+        </figure>
+        <div className="headerPost">
+          <h2>Título da API</h2>
+          <p>
+            Categoria: <span>textoDaAPI</span>
+          </p>
+          <p>
+            Custo estimado: R$<span>textoDaAPI</span>
+          </p>
+          <p>
+            Nível de dificuldade: <span>textoDaAPI</span>
+          </p>
+          <p>
+            Materiais: <span>textoDaAPI</span>
+          </p>
+        </div>
+        <div className="steps">
+            <h3></h3>
+        </div>
+      </StyledIdeaPage>
+    </>
+  );
+};
