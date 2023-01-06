6f0f2a239d2c27e0c0dc4fcb9d1ff0e3aed33555
Danielle da Luz <daniln2209@gmail.com>
2023-01-06 05:47:42 +0000

    feat: style slider fieldset label

--- src/components/Slider/styles.ts
+++ src/components/Slider/styles.ts
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
 export const SliderStyled = styled.fieldset`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 
+  label {
+    color: var(--color-green-100);
+  }
+
+  span {
+    color: var(--color-grey-100);
+  }
+
+  label, span {
+    font-size: var(--font-size-18);
+  }
 `
