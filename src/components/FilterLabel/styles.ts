94bcffbce35cc2432707bcf3c10d9c7d10089078
Danielle da Luz <daniln2209@gmail.com>
2023-01-06 17:52:44 +0000

    feat: add filter's label size

--- src/components/FilterLabel/styles.ts
+++ src/components/FilterLabel/styles.ts
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 
 export const FilterLabelStyled = styled.article`
-  display: flex;
+  align-items: center;
   background-color: var(--color-grey-90);
   border-radius: var(--radius-1);
+  display: flex;
+  gap: 10px;
   justify-content: space-between;
-  padding: 0.313rem 0.438rem;
+  width: fit-content;
+  padding: 0.2rem 0.5rem;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: var(--color-grey-70);    
+  }
+
+  p {
+    font-size: var(--font-size-12);
+  }
   
   p, button {
     color: var(--color-white);
