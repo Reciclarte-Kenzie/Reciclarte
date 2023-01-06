c9119a3661326efea351086544d59ffd1f2de439
Danielle da Luz <daniln2209@gmail.com>
2023-01-06 18:18:53 +0000

    feat: remove label from routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { FilterLabel } from "../components/FilterLabel";
 import { LoginPage } from "../pages/login";
 import { RegisterPage } from "../pages/register";
 import { IdeasProvider } from "../providers/IdeasProvider";
@@ -10,7 +9,6 @@ export const RoutesApp = () => {
     <Routes>
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/label" element={<FilterLabel title="Calça" />} />
       <Route element={<IdeasProvider />}>
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/profile" element={<Profile />} /> */}
