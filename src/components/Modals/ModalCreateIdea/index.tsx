1388f16980cbdda3b15d3f1f41563e51fff546eb
Danielle da Luz <daniln2209@gmail.com>
2023-01-07 05:11:45 +0000

    feat: remove type text declarations

--- src/components/Modals/ModalCreateIdea/index.tsx
+++ src/components/Modals/ModalCreateIdea/index.tsx
@@ -12,13 +12,13 @@ const ModalCreateIdeaBody = () => {
   return (
     <ModalCreateIdeaBodyStyled>
       <section>
-        <Input type="text" placeholder="Insira o título da ideia" label="Título" id="title" />
-        <Input type="text" placeholder="Insira o passo a passo" label="Passo a passo" id="steps" />
+        <Input placeholder="Insira o título da ideia" label="Título" id="title" />
+        <Input placeholder="Insira o passo a passo" label="Passo a passo" id="steps" />
         <Input type="number" placeholder="R$ 0.00" label="Custo estimado" id="estimatedCost" />
       </section>
       <section>
-        <Input type="text" placeholder="Selecione um material" label="Materiais" id="materials" />
-        <Input type="text" placeholder="Insira o título da ideia" label="Título" id="title" />
+        <Input placeholder="Selecione um material" label="Materiais" id="materials" />
+        <Input placeholder="Insira o título da ideia" label="Título" id="title" />
         <Input type="url" placeholder="Insira a URL da foto" label="Foto" id="img" />
       </section>
     </ModalCreateIdeaBodyStyled>
