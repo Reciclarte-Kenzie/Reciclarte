9cbbb2210999f7605cdd5cd920406b87637f4f77
Danielle da Luz <daniln2209@gmail.com>
2023-01-07 15:02:43 +0000

    feat: add input styles on fieldset

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -5,4 +5,23 @@ export const FieldsetStyled = styled.fieldset`
   display: flex;
   flex-direction: column;
   gap: 0.625rem;
+
+  ${styled(Field)`
+    border: 1px solid var(--color-grey-90);
+    border-radius: var(--radius-1);
+    font-weight: 400;
+    font-size: var(--font-size-16);
+    outline: none;
+    padding: 0.9rem 1rem;
+    resize: none;
+
+    ::placeholder {
+      color: var(--color-grey-100);
+    }
+
+    :focus {
+      border: 1px solid var(--color-green-100);
+      outline: none;
+    }
+  `}
 `;
