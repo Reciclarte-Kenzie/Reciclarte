3c88e2cae6fbde8d9c6e39bf49ad5bd459a8aa82
Danielle da Luz <daniln2209@gmail.com>
2023-01-07 05:16:32 +0000

    feat: add slider component

--- src/components/Modals/ModalCreateIdea/index.tsx
+++ src/components/Modals/ModalCreateIdea/index.tsx
@@ -1,4 +1,5 @@
 import { Input } from "../../Input";
+import { Slider } from "../../Slider";
 import { ModalBase } from "../ModalBase";
 import { ModalCreateIdeaBodyStyled } from "./styles";
 
@@ -14,6 +15,7 @@ const ModalCreateIdeaBody = () => {
       <section>
         <Input placeholder="Insira o título da ideia" label="Título" id="title" />
         <Input placeholder="Insira o passo a passo" label="Passo a passo" id="steps" textarea />
+        <Slider defaultValue={2} min={1} max={5} />
         <Input type="number" placeholder="R$ 0.00" label="Custo estimado" id="estimatedCost" />
       </section>
       <section>
