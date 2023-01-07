be19cce387292cde1254ad052395846439fa145a
Danielle da Luz <daniln2209@gmail.com>
2023-01-07 14:53:27 +0000

    feat: add resize and change location of font properties

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,15 +2,18 @@ import styled from "styled-components";
 import { Field } from "./Field";
 
 export const StyledInput = styled(Field)`
-  outline: none;
   border: 1px solid var(--color-grey-90);
   border-radius: var(--radius-1);
+  font-weight: 400;
+  font-size: var(--font-size-16);
+  outline: none;
   padding: 0.9rem 1rem;
+  resize: none;
+
   ::placeholder {
-    font-weight: 400;
-    font-size: var(--font-size-16);
     color: var(--color-grey-100);
   }
+
   :focus {
     border: 1px solid var(--color-green-100);
     outline: none;
