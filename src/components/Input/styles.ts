afb38ad089381102aaa93c417d1cc307dc4a9281
Danielle da Luz <daniln2209@gmail.com>
2023-01-07 15:02:21 +0000

    feat: add fieldset styled component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,27 +1,8 @@
 import styled from "styled-components";
 import { Field } from "./Field";
 
-export const StyledInput = styled(Field)`
-  border: 1px solid var(--color-grey-90);
-  border-radius: var(--radius-1);
-  font-weight: 400;
-  font-size: var(--font-size-16);
-  outline: none;
-  padding: 0.9rem 1rem;
-  resize: none;
-
-  ::placeholder {
-    color: var(--color-grey-100);
-  }
-
-  :focus {
-    border: 1px solid var(--color-green-100);
-    outline: none;
-  }
-`;
-
-export const StyledLabel = styled.label`
-  font-weight: 400;
-  font-size: var(--font-size-18);
-  color: var(--color-green-100);
+export const FieldsetStyled = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  gap: 0.625rem;
 `;
