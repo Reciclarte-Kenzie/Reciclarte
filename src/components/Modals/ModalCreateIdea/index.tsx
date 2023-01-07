f8a096f2a24050c6870333a8d6a515013163270d
Danielle da Luz <daniln2209@gmail.com>
2023-01-07 04:01:16 +0000

    feat: add modal create idea structure

---
+++ src/components/Modals/ModalCreateIdea/index.tsx
@@ -0,0 +1,9 @@
+import { ModalCreateIdeaBodyStyled } from "./styles";
+
+export const ModalCreateIdeaBody = () => {
+  return (
+    <ModalCreateIdeaBodyStyled>
+
+    </ModalCreateIdeaBodyStyled>
+  );
+}
