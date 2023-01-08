b9557912c8fd1d8f9b796599ae49582a9dc060d5
Danielle da Luz <daniln2209@gmail.com>
2023-01-08 17:11:35 +0000

    feat: add select

---
+++ src/components/Input/Select/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+import { LabelStyled } from "../Label/styles";
+import { StyledSelect } from "./style";
+
+interface iSelect extends React.HTMLProps<HTMLSelectElement> {
+  options: {
+    value: string;
+    text: string;
+  }[];
+  placeholder: string;
+  id: string;
+  width?: string;
+  register?: UseFormRegisterReturn;
+  disabled?: boolean;
+  className?: string;
+  label: string;
+  error: string;
+}
+export const Select = ({
+  options,
+  placeholder,
+  id,
+  width,
+  register,
+  disabled,
+  className,
+  label,
+  error
+}: iSelect) => {
+  return (
+    <>
+      <LabelStyled htmlFor={id}>{placeholder}</LabelStyled>
+      <StyledSelect
+        name={id}
+        id={id}
+        width={width}
+        {...register}
+        disabled={disabled}
+        className={className}
+        aria-label={label}
+      >
+        <option value="">{placeholder}</option>
+        {options.map((opt) => {
+          return (
+            <option
+              value={opt.value}
+              key={opt.value}
+              aria-label={`Opção: ${opt.text}`}
+            >
+              {opt.text}
+            </option>
+          );
+        })}
+      </StyledSelect>
+    </>
+  );
+};
