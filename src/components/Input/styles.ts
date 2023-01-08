602f04512b996199ea324f4dbc47d8c87344d11e
Danielle da Luz <daniln2209@gmail.com>
2023-01-08 04:07:38 +0000

    feat: add field styled component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -6,23 +6,4 @@ export const FieldsetStyled = styled.fieldset`
   flex-direction: column;
   gap: 2vh;
   width: 100%;
-
-  input, textarea {
-    border: 1px solid var(--color-grey-90);
-    border-radius: var(--radius-1);
-    font-weight: 400;
-    font-size: var(--font-size-16);
-    outline: none;
-    padding: 0.9rem 1rem;
-    resize: none;
-
-    ::placeholder {
-      color: var(--color-grey-100);
-    }
-
-    :focus {
-      border: 1px solid var(--color-green-100);
-      outline: none;
-    }
-  }
 `;
