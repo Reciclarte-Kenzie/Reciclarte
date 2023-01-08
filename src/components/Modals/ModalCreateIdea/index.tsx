17a3a570baa2df674f6e5af3d3ba3b5d5de4074a
Danielle da Luz <daniln2209@gmail.com>
2023-01-08 03:34:17 +0000

    feat: add materials and categories states

--- src/components/Modals/ModalCreateIdea/index.tsx
+++ src/components/Modals/ModalCreateIdea/index.tsx
@@ -1,32 +1,75 @@
+import { useState } from "react";
 import { Button } from "../../Button";
 import { Input } from "../../Input";
+import { Select } from "../../Input/Select";
 import { Slider } from "../../Slider";
 import { ModalBase } from "../ModalBase";
 import { ModalCreateIdeaBodyStyled } from "./styles";
 
-export const ModalCreateIdea = () => {
-  return (
-    <ModalBase modalTitle="Criar ideia" hideModal={() => undefined} ModalBody={ModalCreateIdeaBody} />
-  );
+interface iSelectOption {
+  value: string;
+  text: string;
 }
 
+export const ModalCreateIdea = () => {
+  return (
+    <ModalBase
+      modalTitle="Criar ideia"
+      hideModal={() => undefined}
+      ModalBody={ModalCreateIdeaBody}
+    />
+  );
+};
+
 const ModalCreateIdeaBody = () => {
+  const [materialsList, setMaterialsList] = useState([] as iSelectOption[]);
+  const [categoriesList, setCategoriesList] = useState([] as iSelectOption[]);
+
+  
   return (
     <ModalCreateIdeaBodyStyled>
       <article>
         <section>
-          <Input placeholder="Insira o título da ideia" label="Título" id="title" />
-          <Input placeholder="Insira o passo a passo" label="Passo a passo" id="steps" textarea />
+          <Input
+            placeholder="Insira o título da ideia"
+            label="Título"
+            id="title"
+          />
+          <Input
+            placeholder="Insira o passo a passo"
+            label="Passo a passo"
+            id="steps"
+            textarea
+          />
           <Slider defaultValue={2} min={1} max={5} />
-          <Input type="number" placeholder="R$ 0.00" label="Custo estimado" id="estimatedCost" />
+          <Input
+            type="number"
+            placeholder="R$ 0.00"
+            label="Custo estimado"
+            id="estimatedCost"
+          />
         </section>
         <section>
-          <Input placeholder="Selecione um material" label="Materiais" id="materials" />
-          <Input placeholder="Insira o título da ideia" label="Título" id="title" />
-          <Input type="url" placeholder="Insira a URL da foto" label="Foto" id="img" />
+          <Select
+            options={}
+            placeholder="Selecione um material"
+            label="Materiais"
+            id="materials"
+          />
+          <Input
+            placeholder="Insira o título da ideia"
+            label="Título"
+            id="title"
+          />
+          <Input
+            type="url"
+            placeholder="Insira a URL da foto"
+            label="Foto"
+            id="img"
+          />
         </section>
       </article>
-      <Button text="Criar" label="Criar ideia"  />
+      <Button text="Criar" label="Criar ideia" />
     </ModalCreateIdeaBodyStyled>
   );
-}
+};
