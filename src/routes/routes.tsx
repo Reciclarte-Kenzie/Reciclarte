7a221f912e55bf576cb0fa4b7e3986e6b53e6ebc
Axionauta <daniln2209@gmail.com>
2023-01-09 01:36:10 -0300

    feat: remove modal from routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { ModalCreateIdea } from "../components/Modals/ModalCreateIdea";
 import { LoginPage } from "../pages/login";
 import Profile from "../pages/profile";
 import { RegisterPage } from "../pages/register";
@@ -12,7 +11,6 @@ export const RoutesApp = () => {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route element={<IdeasProvider />}>
-        <Route path="/modal" element={<ModalCreateIdea />} />
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/profile" element={<Profile />} /> */}
       </Route>
