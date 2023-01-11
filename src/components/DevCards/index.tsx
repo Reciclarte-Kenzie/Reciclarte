db1400b68a1492d33a323015032a35a5e3b974c3
Luís Nunes <luis.ebn98@gmail.com>
2023-01-11 20:00:35 -0300

    feat: add devs info devs card

--- src/components/DevCards/index.tsx
+++ src/components/DevCards/index.tsx
@@ -1,4 +1,4 @@
-export interface iDevCard {
+interface iDevCard {
   dev: {
     avatar_url: string;
     name: string;
@@ -8,6 +8,7 @@ export interface iDevCard {
     bio?: string;
     location?: string;
     blog?: string;
+    position: string;
   };
 }
 
@@ -16,16 +17,26 @@ export const DevCard = ({ dev }: iDevCard) => {
     <section>
       <figure>
         <img src={dev.avatar_url} alt={dev.name} />
-        <figcaption>Foto de perfil</figcaption>
+        <figcaption style={{ display: "none" }}>Foto de perfil</figcaption>
       </figure>
 
       <section className="info">
-        <h3>{dev.name}</h3>
+        <h3>
+          {dev.name} - {dev.position}
+        </h3>
         <section className="subInfo">
           <p>{dev.location}</p>
           <p>{dev.company}</p>
         </section>
-        <a href={dev.html_url}>{dev.login}</a>
+        <section className="externalLinks">
+          <a href={dev.html_url} rel="noopener noreferer" target="blank">
+            {dev.login}
+          </a>
+          <br />
+          <a href={dev.blog} rel="noopener noreferer" target="blank">
+            Linkedin
+          </a>
+        </section>
       </section>
 
       <p>{dev.bio}</p>
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,3 @@ export const api = axios.create({
   baseURL: "https://reciclarte-api.onrender.com",
   timeout: 10000,
 });
-
-export const gitHub = axios.create({
-  baseURL: "https://api.github.com/users/",
-  timeout: 10000,
-});
