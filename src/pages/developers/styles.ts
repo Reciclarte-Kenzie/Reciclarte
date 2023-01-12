0cc3dd6de88bddc3483041eea1c2252cbe2aafbe
Axionauta <daniln2209@gmail.com>
2023-01-11 23:23:59 -0300

    feat: add dev list spacing

--- src/pages/developers/styles.ts
+++ src/pages/developers/styles.ts
@@ -4,4 +4,7 @@ export const DevelopersListStyled = styled.ul`
   display: grid;
   grid-template-columns: repeat(3, 185px);
   gap: 3.75rem 2.5rem;
+  margin: 0 auto;
+  padding: 50px 20px;
+  width: fit-content;
 `
