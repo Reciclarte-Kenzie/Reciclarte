0f984da88d614227e76e26453a3de20c64486d59
Axionauta <daniln2209@gmail.com>
2023-01-11 22:42:32 -0300

    feat: modify dev card structure

--- src/components/DevCards/index.tsx
+++ src/components/DevCards/index.tsx
@@ -1,3 +1,4 @@
+import { StyledButton } from "../Button/styles";
 import { DevCardStyled } from "./styles";
 
 interface iDevCard {
@@ -21,27 +22,26 @@ export const DevCard = ({ dev }: iDevCard) => {
         <img src={dev.avatar_url} alt={dev.name} />
         <figcaption style={{ display: "none" }}>Foto de perfil</figcaption>
       </figure>
-
       <section className="info">
-        <h3>
-          {dev.name} - {dev.position}
-        </h3>
-        <section className="subInfo">
-          <p>{dev.location}</p>
-          <p>{dev.company}</p>
-        </section>
+        <div>
+          <h3>
+            {dev.name}
+          </h3>
+          <p>{dev.position}</p>
+        </div>
         <section className="externalLinks">
-          <a href={dev.html_url} rel="noopener noreferer" target="blank">
-            {dev.login}
-          </a>
-          <br />
-          <a href={dev.blog} rel="noopener noreferer" target="blank">
-            Linkedin
-          </a>
+          <StyledButton>
+            <a href={dev.html_url} rel="noopener noreferer" target="blank">
+              {dev.login}
+            </a>
+          </StyledButton>
+          <StyledButton>
+            <a href={dev.blog} rel="noopener noreferer" target="blank">
+              Linkedin
+            </a>
+          </StyledButton>
         </section>
       </section>
-
-      <p>{dev.bio}</p>
     </DevCardStyled>
   );
 };
